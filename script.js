e0f29a3e3f26ad046f3d5a8c5f782881d29c6ed2
ForssanSeurakunta <aleksi.mertanen@evl.fi>
2025-11-28 12:13:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Tyhjät linkit valmiiksi
    const videoLinks = [
     "https://youtu.be/qyg9VzHkVlU", // 1
-    "", // 2
+    "https://youtu.be/qT9GQKToC_s", // 2
     "", // 3
     "", // 4
     "", // 5
